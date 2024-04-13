49978ad2787585050d686201e833d15f7840f0eb
Tyler Lee <leetyler050@gmail.com>
2024-04-13 12:11:47 -0700

    updated tag names

--- build_remote_backend/build_remote_backend.tf
+++ build_remote_backend/build_remote_backend.tf
@@ -8,7 +8,7 @@
 
 module "s3_and_dynamodb_state_file_builder" {
     source = "../modules/build_state_file_s3_bucket_and_dynamodb"
-    project_name = "github actions, terraform, docker, ecr, ecs, fargate"
+    project_name = "github_actions_terraform_docker_ecr_ecs_fargate"
     region_aws = "us-west-2"
     s3_bucket_name = "github-actions-test-terraform-docker-ecr-ecs-fargate"
     dynamodb_name = "github-actions-fargate-test-db"
