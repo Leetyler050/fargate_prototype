7c86dbd28592f7bbf06dafca2422a8e5c2d14dff
Tyler Lee <leetyler050@gmail.com>
2024-04-13 12:01:47 -0700

    updated actions with oidc and tf backend names

--- build_remote_backend/build_remote_backend.tf
+++ build_remote_backend/build_remote_backend.tf
@@ -1,7 +1,15 @@
+# module "s3_and_dynamodb_state_file_builder" {
+#     source = "../modules/build_state_file_s3_bucket_and_dynamodb"
+#     project_name = "github_actions_test"
+#     region_aws = "us-west-2"
+#     s3_bucket_name = "github-actions-test-s3-bucket"
+#     dynamodb_name = "github-actions-test-db"
+# }
+
 module "s3_and_dynamodb_state_file_builder" {
     source = "../modules/build_state_file_s3_bucket_and_dynamodb"
-    project_name = "github_actions_test"
+    project_name = "github actions, terraform, docker, ecr, ecs, fargate"
     region_aws = "us-west-2"
-    s3_bucket_name = "github-actions-test-s3-bucket"
-    dynamodb_name = "github-actions-test-db"
+    s3_bucket_name = "github-actions-test-terraform-docker-ecr-ecs-fargate"
+    dynamodb_name = "github-actions-fargate-test-db"
 }
