9908019e6656c0d02ffded66a18b55a1e4e3210b
Tyler Lee <leetyler050@gmail.com>
2024-04-15 22:31:52 -0700

    updated vpc variable inputs

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,11 +12,11 @@ module "vpc" {
   aws_region                 = "us-west-2"
   project_name               = "github-actions-terraform-docker-ecr-ecs-fargate"
   infrastructure_environment = "development"
-  public_subnet_numbers = {
+  public_subnet_map = {
                             "us-west-2a" = 1
                             "us-west-2b" = 2
                            }
-  private_subnet_numbers = {
+  private_subnet_map = {
                                 "us-west-2a" = 3
                                 "us-west-2b" = 4
                             }
