1d73c3900a7d7efc46397687eac9933d67a52602
Tyler Lee <leetyler050@gmail.com>
2024-05-02 17:26:16 -0700

    changed vpc_cidr range and input

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,8 @@
 
-#use the vpc module
+#use the infrastructure module
 module "infrastructure" {
   source                     = "../modules/infrastructure"
+  vpc_cidr                   = "10.0.0.0/19"
   aws_region                 = "us-west-2"
   project_name               = "github-actions-terraform-docker-ecr-ecs-fargate"
   infrastructure_environment = "development"
@@ -15,6 +16,7 @@ module "infrastructure" {
                             }
 }
 
+#use the platform module
 module "platform" {
   source                     = "../modules/platform"
   ecs_cluster_name           = "git-act-ter-dock-ecr-ecs-far"
@@ -23,3 +25,19 @@ module "platform" {
   domain_name                = "leetyler050.com"
   public_subnet_set          = module.infrastructure.public_subnets
 }
+
+#use the app_infrastructure module
+# module "app_infrastructure" {
+#   source                     = "../modules/app_infrastructure"
+#   ecs_service_name           = "github-actions-terraform-docker-ecr-ecs-fargate"
+#   docker_image_url           = "leetyler050.com/github-actions-terraform-docker-ecr-ecs-fargate"
+#   memory                     = "512"
+#   docker_container_port      = 80
+#   environment                = "development"
+#   region                     = "us-west-2"
+#   vpc_id                     = module.infrastructure.vpc_id
+#   vpc_cidr_blocks            = module.infrastructure.vpc_cidr
+#   desired_task_number        = 1
+#   ecs_cluster_name           = module.platform.ecs_cluster_name
+#   public_subnet_set          = module.infrastructure.public_subnets
+# }
