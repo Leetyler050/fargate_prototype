5473890b132f7c1b2622246b994268452bb35106
Tyler Lee <leetyler050@gmail.com>
2024-05-16 15:40:36 -0700

    removed the ingress and egress for secuirty group 2

--- modules/platform/security_groups.tf
+++ modules/platform/security_groups.tf
@@ -21,36 +21,36 @@ resource "aws_security_group" "ecs_alb_security_group" {
 }
 
 
-resource "aws_security_group" "ecs_alb_security_group_2" {
-  name        =  "alb-sg-${var.ecs_cluster_name}-2"
-  description =  "Security group for ALB to traffic for ECS cluster"
-  vpc_id      =  var.vpc_id
-
-
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "TCP"
-        cidr_blocks = [var.internet_cidr_blocks]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = [var.internet_cidr_blocks]
-    }
-
-}
-
 # resource "aws_security_group" "ecs_alb_security_group_2" {
 #   name        =  "alb-sg-${var.ecs_cluster_name}-2"
 #   description =  "Security group for ALB to traffic for ECS cluster"
 #   vpc_id      =  var.vpc_id
 
 
-#     ingress = []
+#     ingress {
+#         from_port   = 443
+#         to_port     = 443
+#         protocol    = "TCP"
+#         cidr_blocks = [var.internet_cidr_blocks]
+#     }
 
-#     egress = []
+#     egress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = [var.internet_cidr_blocks]
+#     }
 
 # }
+
+resource "aws_security_group" "ecs_alb_security_group_2" {
+  name        =  "alb-sg-${var.ecs_cluster_name}-2"
+  description =  "Security group for ALB to traffic for ECS cluster"
+  vpc_id      =  var.vpc_id
+
+
+    ingress = []
+
+    egress = []
+
+}
