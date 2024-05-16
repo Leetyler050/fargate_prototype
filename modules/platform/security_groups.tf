39e49dd91dc218fbe5ca2b04e79fc71a85fdca84
Tyler Lee <leetyler050@gmail.com>
2024-05-16 15:15:52 -0700

    testing out deleting security group

--- modules/platform/security_groups.tf
+++ modules/platform/security_groups.tf
@@ -1,21 +1,32 @@
+# resource "aws_security_group" "ecs_alb_security_group" {
+#   name        =  "alb-sg-${var.ecs_cluster_name}"
+#   description =  "Security group for ALB to traffic for ECS cluster"
+#   vpc_id      =  var.vpc_id
+
+
+#     ingress {
+#         from_port   = 443
+#         to_port     = 443
+#         protocol    = "TCP"
+#         cidr_blocks = [var.internet_cidr_blocks]
+#     }
+
+#     egress {
+#         from_port   = 0
+#         to_port     = 0
+#         protocol    = "-1"
+#         cidr_blocks = [var.internet_cidr_blocks]
+#     }
+
+# }
+
 resource "aws_security_group" "ecs_alb_security_group" {
   name        =  "alb-sg-${var.ecs_cluster_name}"
   description =  "Security group for ALB to traffic for ECS cluster"
   vpc_id      =  var.vpc_id
 
+    ingress = []
 
-    ingress {
-        from_port   = 443
-        to_port     = 443
-        protocol    = "TCP"
-        cidr_blocks = [var.internet_cidr_blocks]
-    }
-
-    egress {
-        from_port   = 0
-        to_port     = 0
-        protocol    = "-1"
-        cidr_blocks = [var.internet_cidr_blocks]
-    }
+    egress = []
 
 }
