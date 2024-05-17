a6c228c8c756c7ce3437c3f7f8d7a9e235d6893f
Tyler Lee <leetyler050@gmail.com>
2024-05-16 22:53:49 -0700

    fix to security group

--- modules/app_infrastructure/fargate_security_group.tf
+++ modules/app_infrastructure/fargate_security_group.tf
@@ -1,16 +1,41 @@
-resource "aws_security_group" "lb_security_group" {
-    name       =  "app-sg-${var.ecs_service_name}"
-    description =  "Security group for app to traffic for ECS cluster"
+# resource "aws_security_group" "lb_security_group" {
+#     name       =  "app-sg-${var.ecs_service_name}"
+#     description =  "Security group for app to traffic for ECS cluster"
+#     vpc_id = var.vpc_id
+
+#     ingress {
+#         from_port = 8050
+#         protocol = "TCP"
+#         to_port = 8050
+#         cidr_blocks = [var.vpc_cidr_blocks]
+#     }
+
+#     egress {
+#         from_port = 0
+#         protocol = "-1"
+#         to_port = 0
+#         cidr_blocks = ["0.0.0.0/0"] #var.vpc_cidr_blocks
+#     }
+
+#     tags = {
+#         Name = "app-sg-${var.ecs_service_name}"
+#     }
+# }
+
+resource "aws_security_group" "ecs_security_group" {
+    name       =  "ecs-sg-${var.ecs_service_name}"
+    description =  "Security group for ECS cluster"
     vpc_id = var.vpc_id
 
     ingress {
         from_port = 8050
-        protocol = "TCP"
-        to_port = 8050
+        protocol = "-1"
+        to_port = 0
         cidr_blocks = [var.vpc_cidr_blocks]
+        #security_groups = [aws_security_group.lb_security_group.id]
     }
 
-    egress {
+        egress {
         from_port = 0
         protocol = "-1"
         to_port = 0
@@ -20,23 +45,5 @@ resource "aws_security_group" "lb_security_group" {
     tags = {
         Name = "app-sg-${var.ecs_service_name}"
     }
+
 }
-
-# resource "aws_security_group" "ecs_security_group" {
-#     name       =  "ecs-sg-${var.ecs_service_name}"
-#     description =  "Security group for ECS cluster"
-#     vpc_id = var.vpc_id
-
-#     ingress {
-#         from_port = 0
-#         protocol = "-1"
-#         to_port = 0
-#         cidr_blocks = [var.vpc_cidr_blocks]
-#         security_groups = [aws_security_group.lb_security_group.id]
-#     }
-
-#     egress {
-#         from_port = 0
-#         protocol = "-1"
-#         to_port = 0
-#         cidr_blocks = ["
