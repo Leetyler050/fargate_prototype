4377ede9d0889a3836dc6180218842e153775a6a
Tyler Lee <leetyler050@gmail.com>
2024-05-17 22:05:50 -0700

    readded listener rule

--- modules/app_infrastructure/app_load_balancer.tf
+++ modules/app_infrastructure/app_load_balancer.tf
@@ -20,20 +20,19 @@ resource "aws_lb_target_group" "ecs_app_target_group" {
     }
 }
 
-#removed listener rule to update target group
-# resource "aws_lb_listener_rule" "ecs_lb_listener_rule" {
-#     listener_arn = var.ecs_aws_lb_listener_arn
+resource "aws_lb_listener_rule" "ecs_lb_listener_rule" {
+    listener_arn = var.ecs_aws_lb_listener_arn
 
-#     action {
-#         type             = "forward"
-#         target_group_arn = aws_lb_target_group.ecs_app_target_group.arn
-#     }
-#     condition {
-#         host_header {
-#             values = ["${lower(var.ecs_service_name)}.${var.domain_name}"]
-#     }
-#     }
-# }
+    action {
+        type             = "forward"
+        target_group_arn = aws_lb_target_group.ecs_app_target_group.arn
+    }
+    condition {
+        host_header {
+            values = ["${lower(var.ecs_service_name)}.${var.domain_name}"]
+    }
+    }
+}
 
 
 
