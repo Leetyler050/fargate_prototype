d5319c57b37de6bc0c8aabcab90f37833e94cdb6
Tyler Lee <leetyler050@gmail.com>
2024-05-27 16:53:06 -0700

    updated port for subdomain listener

--- modules/child_hosted_zone/sub_domain_listener.tf
+++ modules/child_hosted_zone/sub_domain_listener.tf
@@ -1,7 +1,7 @@
 #Creates a listener for the sub domain for the load balancer
 resource "aws_lb_listener" "sub_domain_ecs_lb_https_listener" {
     load_balancer_arn = var.load_balancer_arn
-    port = 443
+    port = 8443
     protocol = "HTTPS"
     ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
     certificate_arn = aws_acm_certificate.sub_domain_certificate.arn
