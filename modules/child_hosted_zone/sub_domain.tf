fadad31c9ac1b210d1eeb51daaaf05b3220c1de5
Tyler Lee <leetyler050@gmail.com>
2024-05-27 19:56:54 -0700

    changed from alias to cname

--- modules/child_hosted_zone/sub_domain.tf
+++ modules/child_hosted_zone/sub_domain.tf
@@ -42,14 +42,22 @@ resource "aws_acm_certificate_validation" "sub_domain_certificate_validation" {
   validation_record_fqdns = [aws_route53_record.sub_domain_cert_validation_record.fqdn]
 }
 
-resource "aws_route53_record" "sub_domain_load_balancer_record" {
-    name = "*.${var.sub_domain_name}"
-    type = "A"
-    zone_id = aws_route53_zone.sub_domain.zone_id
+# resource "aws_route53_record" "sub_domain_load_balancer_record" {
+#     name = "*.${var.sub_domain_name}"
+#     type = "A"
+#     zone_id = aws_route53_zone.sub_domain.zone_id
+#     #ttl = 60
+#     alias {
+#         name = var.aws_lb_ecs_cluster_lb_dns_name
+#         zone_id = var.aws_lb_ecs_cluster_lb_zone_id
+#         evaluate_target_health = false
+#     }
+# }
 
-    alias {
-        name = var.aws_lb_ecs_cluster_lb_dns_name
-        zone_id = var.aws_lb_ecs_cluster_lb_zone_id
-        evaluate_target_health = false
-    }
+resource "aws_route53_record" "cname_load_balancer_record" {
+  zone_id =  aws_route53_zone.sub_domain.zone_id # Replace with your zone ID
+  name    = "*.${var.sub_domain_name}" # Replace with your subdomain, Note: not valid with "apex" domains, e.g. example.com
+  type    = "CNAME"
+  ttl     = "60"
+  records = [var.aws_lb_ecs_cluster_lb_dns_name]
 }
