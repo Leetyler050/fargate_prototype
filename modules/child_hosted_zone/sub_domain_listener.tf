104d93083f0fa9e2ed43902b9038dba890cd4118
Tyler Lee <leetyler050@gmail.com>
2024-05-28 22:51:55 -0700

    readd in listeners and rules

--- modules/child_hosted_zone/sub_domain_listener.tf
+++ modules/child_hosted_zone/sub_domain_listener.tf
@@ -1,28 +1,28 @@
-#Creates a listener for the sub domain for the load balancer
-# resource "aws_lb_listener" "sub_domain_ecs_lb_https_listener" {
-#     load_balancer_arn = var.load_balancer_arn
-#     port = 443
-#     protocol = "HTTPS"
-#     ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
-#     certificate_arn = aws_acm_certificate.sub_domain_certificate.arn
+##Creates a listener for the sub domain for the load balancer
+resource "aws_lb_listener" "sub_domain_ecs_lb_https_listener" {
+    load_balancer_arn = var.load_balancer_arn
+    port = 443
+    protocol = "HTTPS"
+    ssl_policy = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+    certificate_arn = aws_acm_certificate.sub_domain_certificate.arn
 
-#     default_action {
-#         type = "forward"
-#         target_group_arn = var.load_balancer_target_group_arn
-#     }
-# }
+    default_action {
+        type = "forward"
+        target_group_arn = var.load_balancer_target_group_arn
+    }
+}
 
-# ##listener rule commenting out for now
-# resource "aws_lb_listener_rule" "ecs_lb_listener_rule" {
-#     listener_arn = aws_lb_listener.sub_domain_ecs_lb_https_listener.arn
+##listener rule commenting out for now
+resource "aws_lb_listener_rule" "ecs_lb_listener_rule" {
+    listener_arn = aws_lb_listener.sub_domain_ecs_lb_https_listener.arn
 
-#     action {
-#         type             = "forward"
-#         target_group_arn = var.aws_lb_target_group_ecs_app_target_group_arn
-#     }
-#     condition {
-#         host_header {
-#             values = ["sub-fargate-app.${var.sub_domain_name}.${var.main_domain_name}"]
-#     }
-#     }
-# }
+    action {
+        type             = "forward"
+        target_group_arn = var.aws_lb_target_group_ecs_app_target_group_arn
+    }
+    condition {
+        host_header {
+            values = ["sub-fargate-app.${var.sub_domain_name}.${var.main_domain_name}"]
+    }
+    }
+}
